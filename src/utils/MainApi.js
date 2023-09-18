16d9b30a7c2bca6d3fd604610fe7bf85ed374fe1
evgeny <evgeny@158.160.8.209>
2023-09-18 23:42:14 +0300

    dadf

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://frontend-evgeny.nomoredomains.sbs";
 
 export function register({ name, email, password }) {
     return fetch(`${BASE_URL}/signup`, {
